ed59ee48cb719b32be79427c184b8507387f1607
sakshi123-bil <sakshipadamwar2002@gmail.com>
2021-09-11 14:48:39 +0530

    news reader

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React,{useState, useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
 import './App.css';
+import useStyles from './styles';
+import  NewsCards  from './components/NewsCards/NewsCards';
 
-function App() {
+
+
+const alankey="a58726c5c7776fcc04d3076aca7f17162e956eca572e1d8b807a3e2338fdd0dc/stage";
+
+
+const App=()=> {
+  const [newsArticles,setNewsArticles]=useState([]);
+  const classes=useStyles();
+
+  useEffect(() => {
+    alanBtn({
+      key: alankey,
+      onCommand: ({ command,articles}) =>{
+        if(command ==="newHeadlines"){
+         setNewsArticles(articles);
+        }
+      }
+    })
+  },[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{paddingLeft:'45%'}}>News Reader</h1>
+      <NewsCards articles={newsArticles} />
     </div>
   );
 }
